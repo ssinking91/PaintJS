bf455d0a5453d58f8726117772194db0749a125c
Shin_HangMin <tlsgkdals71@educere.biz>
2022-06-29 16:40:39 +0900

    Docs: README 수정

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+// fillStyle = color : 도형을 채우는 색을 설정합니다.
+// fillRect(x, y, width, height) : 색칠된 직사각형을 그립니다.
+// strokeStyle = color : 도형의 윤곽선 색을 설정합니다.
+// lineWidth = value : 이후 그려질 선의 두께를 설정합니다.
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
@@ -28,7 +32,26 @@ function startPainting() {
   painting = true;
 }
 
+// ✔️ 경로 그리기
+// 1. 경로를 생성합니다.
+// 2. 그리기 명령어를 사용하여 경로상에 그립니다.
+// 3. 경로가 한번 만들어졌다면, 경로를 렌더링 하기 위해서 윤곽선을 그리거나 도형 내부를 채울수 있습니다.
+
+// 1. beginPath() : 새로운 경로를 생성
+// 2-1. moveTo(x, y) : 펜을  x와 y 로 지정된 좌표로 옮김
+// 2-2. lineTo(x, y) : 현재의 드로잉 위치에서 x와 y로 지정된 위치까지 선을 그림
+// 3-1. stroke() : 윤곽선을 이용하여 도형을 그림
+// 3-2. fill() : 경로의 내부를 채워서 내부가 채워진 도형을 그림
+
+// 참고:  현재 열린 path가  비어있는 경우 ( beginPath() 메소드를 사용한 직 후, 혹은캔버스를 새로 생성한 직후), 첫 경로 생성 명령은 실제 동작에 상관 없이 moveTo()로 여겨지게 됩니다. 그렇기 때문에 경로를 초기화한 직후에는 항상 명확하게 시작 위치를 설정해 두는것이 좋습니다.
+// 참고:  fill() 메소드 호출 시, 열린 도형은 자동으로 닫히게 되므로  closePath()메소드를 호출하지 않아도 됩니다. 이것은 stroke() 메소드에는 적용되지 않습니다.
+// 참고:  closePath() : 현재 점 위치와 시작점 위치를 직선으로 이어서 도형을 닫음. 이미 도형이 닫혔거나 한 점만 존재한다면, 이 메소드는 아무것도 하지 않음
+// 참고 : offsetX, offsetY => 이벤트 대상이 기준
+//        - offsetX : 이벤트 대상 객체에서의 상대적 마우스 x좌표 위치를 반환
+//        - offsetY : 이벤트 대상 객체에서의 상대적 마우스 y좌표 위치를 반환
+
 function onMouseMove(event) {
+  console.log(event.offsetX, event.offsetY);
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
@@ -63,15 +86,18 @@ function handleModeClick() {
 
 function handleCanvasClick() {
   if (filling) {
+    // fillRect(x, y, width, height) : 색칠된 직사각형을 그립니다.
     ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
 }
 
+// 마우스 우클릭 방지
 function handleCM(event) {
   event.preventDefault();
 }
 
 function handleSaveClick() {
+  // canvas.toDataURL() : 마우스 오른쪽 단추로 클릭해 메뉴를 열 때 발생.
   const image = canvas.toDataURL();
   const link = document.createElement("a");
   link.href = image;
@@ -85,10 +111,12 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+
+  // contextmenu : 마우스 오른쪽 메뉴 선택 이벤트
   canvas.addEventListener("contextmenu", handleCM);
 }
 
-Array.from(colors).forEach(color =>
+Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
 
